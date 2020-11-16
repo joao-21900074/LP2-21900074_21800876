2ad5418d65c8ec0f917e757d36a443f78e6cba90
Matheus-21800876 <a21800876@alunos.ulht.pt>
2020-11-16 18:30:43 +0000

    Função startGame criada

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/Main.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Main.java
@@ -1,7 +1,10 @@
 package pt.ulusofona.lp2.theWalkingDEISIGame;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        File ficheiro = new File("Game_sample.txt");
+        TWDGameManager.startGame(ficheiro);
     }
 }
