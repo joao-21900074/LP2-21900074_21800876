4b3e1d37bc187b6590f0b4c74169abf50f05c5d3
Matheus-21800876 <a21800876@alunos.ulht.pt>
2020-12-19 20:15:29 +0000

    Leitura de Ficheiro V1
    (Falta passar os dados para os respectivos Objetos)

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/Main.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/Main.java
@@ -1,6 +1,11 @@
 package pt.ulusofona.lp2.theWalkingDEISIGame;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        TWDGameManager testStart = new TWDGameManager();
+        File ficheiro = new File("Game_sample.txt");
+        testStart.startGame(ficheiro);
     }
 }
