5af9dac62c6a40e39fc95ca1334db5e88dce2288
Matheus-21800876 <a21800876@alunos.ulht.pt>
2021-01-21 20:40:45 +0000

    Ordem de erros alterada

--- src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
+++ src/pt/ulusofona/lp2/theWalkingDEISIGame/TWDGameManager.java
@@ -68,11 +68,7 @@ public class TWDGameManager {
                 //Quantidade de Criaturas
                 case 3:
                     int nCriaturas = Integer.parseInt(info[0]);
-
-                    //Necssário pelo menos 2 criaturas para jogar o jogo
-                    if(nCriaturas < 3){
-                        throw new InvalidTWDInitialFileException(nCriaturas);
-                    }
+                    int recordNCriaturas = nCriaturas;
 
                     //Criação das Criaturas
                     while (nCriaturas != 0) {
@@ -104,6 +100,12 @@ public class TWDGameManager {
 
                         nCriaturas--;
                     }
+
+                    //Necssário pelo menos 2 criaturas para jogar o jogo
+                    if(recordNCriaturas < 3){
+                        throw new InvalidTWDInitialFileException(nCriaturas);
+                    }
+
                     break;
 
                 //Quantidade de Equipamentos
